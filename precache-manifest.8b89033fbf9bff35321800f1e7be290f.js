61f522af0672b7e60c371e96bead0f368dc254f7
Max Vorozhcov <max@lookatme.ru>
2019-07-23 10:48:07 +0300

    deploy

---
+++ precache-manifest.8b89033fbf9bff35321800f1e7be290f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d9ff55bff71ef13b7afcc168aa9ef4",
+    "url": "/posts-breaking/index.html"
+  },
+  {
+    "revision": "ee39ec255e394016ab02",
+    "url": "/posts-breaking/static/css/main.7c0dd6c1.chunk.css"
+  },
+  {
+    "revision": "f7ae928bdc435477c7ca",
+    "url": "/posts-breaking/static/js/2.03e9c544.chunk.js"
+  },
+  {
+    "revision": "ee39ec255e394016ab02",
+    "url": "/posts-breaking/static/js/main.5add1a3b.chunk.js"
+  },
+  {
+    "revision": "4b33e0d2babfe09f461b",
+    "url": "/posts-breaking/static/js/runtime~main.32784df0.js"
+  }
+]);
